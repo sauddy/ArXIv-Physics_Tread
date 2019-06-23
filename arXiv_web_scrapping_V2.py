94b1fb0870975efbde54bcfcf300148820cfc796
sauddy <sayanatanauddy21@gmail.com>
2019-06-23 13:29:56 +0800

    minor edits to generalize the code

--- arXiv_web_scrapping_V2.py
+++ arXiv_web_scrapping_V2.py
@@ -1,7 +1,7 @@
 '''
 About: This script scrapes the ArXiv for recent and old papers. We are only considering Physics papers in the last 5 years.
-Author: Ramit Dey and Sayantan Auddy
-Created : 4 June 2019
+Author:Sayantan Auddy and Ramit Dey
+Created : 23 June 2019
 
 Input: The category 
 	   The year range for which the data is needed
@@ -33,16 +33,16 @@ for year_number in range(start_year,end_year): ## the last year in not included
 	csv_writer.writerow(['Title','Authors'])
 
 
-	# skip_list = ['0','2000','4000']
+	skip_list = ['0','2000','4000']
 
 	## for shorter list just use '0'
-	skip_list = ['0']
+	# skip_list = ['0']
 	for skip in skip_list: ## this loops over next page specified by the skip valuerange in ArXiv .
 		
 
 		'''Requesting the required webpage'''
-		# source=requests.get('https://arxiv.org/list/'+ category +'/'+ year +'?skip='+ skip +'&show=2000').text
-		source=requests.get('https://arxiv.org/list/gr-qc/18').text
+		source=requests.get('https://arxiv.org/list/'+ category +'/'+ year +'?skip='+ skip +'&show=2000').text
+		
 		'''souping the html'''
 		soup=BeautifulSoup(source,'lxml')
 
@@ -58,13 +58,8 @@ for year_number in range(start_year,end_year): ## the last year in not included
 		authors = content.find('div',class_='list-authors')
 		# print(title.text)
 		# print(authors.text)
-		# article_authors = authors.text.split(':')
-		# article_title =title.text.split(':')
-		# print(article_title)
 		
-
-
-		## extracting the titles from each row within the dl tag where the title is 
+	    ## extracting the titles from each row within the dl tag where the title is 
 		## given the class, list-title mathjax 	
 		## Also extracting the authors list corresponding to the given title
 		
@@ -72,7 +67,7 @@ for year_number in range(start_year,end_year): ## the last year in not included
 		for title, authors in zip(content.find_all('div',class_='list-title mathjax'),content.find_all('div',class_='list-authors')):
 			article_title =title.text.split(':') ######### TO split the title line from ':'	
 			article_authors = authors.text.split(':')
-			
+			# print(article_author)
 			csv_writer.writerow([":".join(article_title[1:]),article_authors[1]]) ### To get the title text only and not the word "Title" and only the authors
 	csv_file.close()
 	print("The CSV file is ready ")
